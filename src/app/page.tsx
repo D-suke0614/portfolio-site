e85e0af074cd6009d6380a2fe6d94cc1227e2d70
Daisuke <d.suke.kida@gmail.com>
2024-03-15 23:57:36 +0900

    feat: contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Contact from "./components/contact/Contact";
 import Mv from "./components/mv/Mv";
 
 export default function Home() {
   return (
     <main className="app">
       <Mv />
+      <Contact />
     </main>
   );
 }
