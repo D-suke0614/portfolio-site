e6864434cbd59130c0ebb2c8325f0f3af9acbcf6
Daisuke <d.suke.kida@gmail.com>
2024-03-31 21:49:27 +0900

    wip: work section

--- src/app/components/contact/Contact.tsx
+++ src/app/components/contact/Contact.tsx
@@ -2,7 +2,7 @@ import Section from "../section/Section";
 
 const Contact = () => {
   return (
-    <Section sectionId="contact" title={"Contact"} bg={"gray"}>
+    <Section sectionId="contact" title={"Contact"} bg={"white"}>
       <div>
         <p>お仕事等のお問い合わせは、XのDMよりお願いします。</p>
       </div>
